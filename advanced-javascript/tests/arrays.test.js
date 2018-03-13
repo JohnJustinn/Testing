ba98e845050f2bcdd1f1d171116c147df4a6f18a
Johnathan Justinn <jjustinn85@gmail.com>
2018-03-13 15:31:13 -0400

    Working on array, down to reduce

--- advanced-javascript/tests/arrays.test.js
+++ advanced-javascript/tests/arrays.test.js
@@ -17,46 +17,76 @@ chai.use(sinonChai);
 // hint 3. - if the function you're testing requires a * callback *, make sure you use a spy like sinon
 
 describe('Arrays', () => {
+  let testArray = [];
+  beforeEach(() => {
+    testArray = ['First', 'Second', 'Third', 'Fourth', 'Fifth'];
+  });
+  afterEach(() => {
+    testArray = [];
+  });
+
   describe('`each`', () => {
     it('should be a function', () => {
       const each = arrayFunctions.each;
-      expect(each).to.be.a('string');
+      expect(each).to.be.a('function');
     });
-    // begin here
-  });
+    it('Should be a callback for items in array'), () => {
+      const spy = sinon.spy();
+      const callTimes = testArray.length;
+      arrayFunctions.each(testArray, spy);
+      expect(spy).to.have.callCount(callTimes);
+    }
+  })
 
   describe('`map`', () => {
+    const map = arrayFunctions.map;
     it('should be a function', () => {
-      const map = arrayFunctions.map;
-      expect(map).to.be.an('object');
+      expect(map).to.be.an('function');
+    });
+    it('Should expect a callback', () => {
+      const cb = sinon.spy();
+      const callTimes = testArray.length;
+      arrayFunctions.map(testArray, cb);
+      expect(cb).to.have.callCount(callTimes);
+    });
+    it('Should return a new array', () => {
+      const mapArray = map(testArray, cb);
+      expect(mapArray).to.have.lengthOf(testArray.length);
+      expect(mapArray).to.not.equal(testArray);
     });
   });
 
   describe('`reduce`', () => {
+    const reduce = arrayFunctions.reduce;
     it('should be a function', () => {
-      const reduce = arrayFunctions.reduce;
-      expect(reduce).to.be.a('number');
+      expect(reduce).to.be.a('function');
+    });
+    it('Should expect a callback', () => {
+      const cb = sinon.spy();
+      const callTimes = testArray.length;
+      arrayFunctions.reduce(testArray, cb);
+      expect(cb).to.have.callCount(callTimes);
     });
   });
 
   describe('`find`', () => {
     it('should be a function', () => {
       const find = arrayFunctions.find;
-      expect(find).to.be.an('array');
+      expect(find).to.be.an('function');
     });
   });
 
   describe('`filter`', () => {
     it('should be a function', () => {
       const filter = arrayFunctions.filter;
-      expect(filter).to.be.a('null');
+      expect(filter).to.be.a('function');
     });
   });
 
   describe('`flatten`', () => {
     it('should be a function', () => {
       const flatten = arrayFunctions.flatten;
-      expect(flatten).to.be.a('promise');
+      expect(flatten).to.be.a('function');
     });
   });
 });
